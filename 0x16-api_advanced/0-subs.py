67b5aaa2c4f4b4b960257e8e28bdc948fdb46dcf
Yakub289 <Yakubsulleiman24@gmail.com>
2024-03-27 17:59:50 -0700

    Indicate total number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,12 @@ for a given subreddit.If an invalid subreddit is given,
 the function should return 0."""
 
 
+import requests
+
+
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit required to command the process"""
-    import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
