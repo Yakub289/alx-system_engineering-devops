1aa9ab00526335335766934d44bcb493a1d7d216
Yakub289 <Yakubsuleiman24@gmail.com>
2024-04-22 23:48:38 +0000

    Tracking,Trafficking and Modelling stack flow

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Simulate HTTP requests to a web server to minimalize failure.
+
+{'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
